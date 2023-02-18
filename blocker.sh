4dba24aa6e4a807a53677a7a9fa1f23bdf410b4f
Airlangga Permana Putra <airlanggapermana96@gmail.com>
2023-02-19 03:31:31 +0700

    Update blocker.sh

--- blocker.sh
+++ blocker.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# Block bad bots based on user agents, IP addresses, and referrers
-
 # Read user agents from ua.txt
 while read -r user_agent; do
   iptables -A INPUT -p tcp --dport 80 -m string --string "$user_agent" --algo bm -j DROP
   iptables -A INPUT -p tcp --dport 443 -m string --string "$user_agent" --algo bm -j DROP
+  echo "Blocking user agent: $user_agent"
 done < ua.txt
 
 # Read IP addresses from ip.txt
 while read -r ip_address; do
   iptables -A INPUT -s "$ip_address" -j DROP
+  echo "Blocking IP address: $ip_address"
 done < ip.txt
 
 # Read referrers from referer.txt
 while read -r referrer; do
   iptables -A INPUT -p tcp --dport 80 -m string --string "$referrer" --algo bm -j DROP
   iptables -A INPUT -p tcp --dport 443 -m string --string "$referrer" --algo bm -j DROP
+  echo "Blocking referrer: $referrer"
 done < referer.txt
+
+echo "Done Progress"
